4fd8e3dc3931a08b9170f4749dbc1c99640a0ff1
Orlando Ernesto Abreu Cruz <orlye.ac@gmail.com>
2025-01-19 15:54:14 -0500

    [ i ] modificado comentado modulo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,27 +5,27 @@ import java.util.List;
 
 public class Main {
 
-    public static int add(int a,  int b) {
+    public static int add(int a,  int b) { // función sobrecargada
         return a + b;
     }
 
-    public static int add(int a,  int b, int c) {
+    public static int add(int a,  int b, int c) { // función sobrecargada
         return a + b + c;
     }
 
     public static void main(String[] args) {
 
-        MiClase miObjeto = new MiClase(0, 0);
+        MiClase miObjeto = new MiClase(0, 0); // una instancia
 
-        // miObjeto.miPrimerAtributo = 4;
-        miObjeto.setMiPrimerAtributo(3);
+        // miObjeto.miPrimerAtributo = 4; // acceso con el operador punto no es posible para un miembro privado de la instancia
+        miObjeto.setMiPrimerAtributo(3); // acceso utilizando la función setter de la instancia
 
         // miObjeto.miSegundoAtributo = 1;
-        // miObjeto.setMiSegundoAtributo(1);
+        // miObjeto.setMiSegundoAtributo(1); // acceso con la función setter quitado declarando el miembro final
 
-        System.out.println(miObjeto.getMiPrimerAtributo());
+        System.out.println(miObjeto.getMiPrimerAtributo()); // acceso con la función getter de la instancia
 
-        int p = 3;
+        int p = 3;  // ejemplo de sobrecarga de función
         int q = 1;
         int o = 1;
         int t = add(p, q, o);
